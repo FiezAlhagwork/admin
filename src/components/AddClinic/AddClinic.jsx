9daac8e9f30c9b252e5126e61108e73ecd3dbcce
FiezAlhagwork <148772477+FiezAlhagwork@users.noreply.github.com>
2024-09-26 11:12:52 +0300

    init

--- src/components/AddClinic/AddClinic.jsx
+++ src/components/AddClinic/AddClinic.jsx
@@ -110,7 +110,7 @@ export const AddClinic = ({ city }) => {
     top: "50%",
     left: "50%",
     transform: "translate(-50%, -50%)",
-    width: 450,
+    width: 400,
     bgcolor: "background.paper",
     border: "2px solid #000",
     boxShadow: 24,
