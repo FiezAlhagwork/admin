9b7ef8551d000b280ce7dc4bbc8531845b077a4e
FiezAlhagwork <148772477+FiezAlhagwork@users.noreply.github.com>
2024-09-26 11:18:00 +0300

    init

--- src/components/AddAdmin/AddAdmin.jsx
+++ src/components/AddAdmin/AddAdmin.jsx
@@ -35,7 +35,7 @@ const AddAdmin = ({city}) => {
     top: "50%",
     left: "50%",
     transform: "translate(-50%, -50%)",
-    width: 450,
+    width: 400,
     bgcolor: "background.paper",
     border: "2px solid #000",
     boxShadow: 24,
